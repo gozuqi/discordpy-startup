4695af24aa79eb8f6b5b5480977ad3a8d465387a
Fio ♪ <fio@FionoMacBook-Pro.local>
2021-05-20 18:37:35 +0900

    gogo

--- discordbot.py
+++ discordbot.py
@@ -14,8 +14,8 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def gogo(ctx):
+    await ctx.send('gogogo')
 
 
 bot.run(token)
