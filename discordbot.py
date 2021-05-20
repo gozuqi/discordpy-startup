60cf9d3f03a36cda0ae40ead48b878645b4e4f89
Fio ♪ <fio@FionoMacBook-Pro.local>
2021-05-20 18:56:50 +0900

    commit

--- discordbot.py
+++ discordbot.py
@@ -30,6 +30,16 @@ async def on_ready():
     print('------')
 
 
+# 返信する非同期関数を定義
+async def reply(message):
+    reply = f'{message.author.mention} ウェイヨー' # 返信メッセージの作成
+    await message.channel.send(reply) # 返信メッセージを送信
+
+# 発言時に実行されるイベントハンドラを定義
+@client.event
+async def on_message(message):
+    if client.user in message.mentions: # 話しかけられたかの判定
+        await reply(message) # 返信する非同期関数を実
 
 
 
